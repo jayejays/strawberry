71d2610f67d7e677d1ac548b4fb96bf3a58d9469
Jason <jasonmadrigal96@gmail.com>
2017-07-27 18:41:11 +1200

    Added tab pages and integrated camera

---
+++ strawberry/strawberry/strawberry/AzureTable.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace strawberry
+{
+    public partial class AzureTable : ContentPage
+    {
+        public AzureTable()
+        {
+            InitializeComponent();
+        }
+    }
+}
